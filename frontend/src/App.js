002564cafcd7320caccb6e926caf211636704260
tummanoon <kanajan2562@gmail.com>
2021-05-13 16:56:20 +0700

    2.57.26

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
+import{BrowserRouter, Route} from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -16,41 +21,16 @@ function App() {
       </header>
 
       <main>
-
-        <div className="row center">
-          {
-            data.products.map((product)=>(
-            
-              <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img className="medium" 
-                src={product.image} 
-                alt={product.name} 
-                />
-              </a>
-              <div className="card-body">
-              <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                </div>
-                <div className="price">${product.price}</div>
-              </div>
-            </div>
-            ))
-          }
-
-        </div>
+        <Route path="/product/:id" component ={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+     
       </main>
 
       <footer className="row center">All right reserved </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+

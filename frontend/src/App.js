d6773f819dfe8d45e09f28aca39568458a66f6e7
tummanoon <kanajan2562@gmail.com>
2021-05-14 21:53:01 +0700

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import Product from './components/Product';
 import data from './data';
-import{BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import ProductScreen from './screens/ProductScreen';
 import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/ ">amazona</a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
 
-      <main>
-        <Route path="/product/:id" component ={ProductScreen}></Route>
-        <Route path="/" component={HomeScreen} exact></Route>
-     
-      </main>
+            <Link to="/signin">Sign In</Link>
+          </div>
+        </header>
 
-      <footer className="row center">All right reserved </footer>
-    </div>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+
+        </main>
+
+        <footer className="row center">All right reserved </footer>
+      </div>
     </BrowserRouter>
   );
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-
+import {cartReducer} from './reducers/cartReducers';
 
 import {
     productDetailsReducer,
@@ -8,10 +8,18 @@ import {
 
 } from './reducers/productReducers';
 
-const initialState = {};
+const initialState = {
+    cart:{
+        cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        :[],
+    },
+};
+
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart:cartReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
